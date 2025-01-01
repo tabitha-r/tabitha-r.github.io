ec587f31f8f4a61a3abf9b10a3e4e2756423cccb
tabitha-r <93261325+tabitha-r@users.noreply.github.com>
2025-01-01 11:35:49 +0000

    Fix CSS

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import './assets/sass/index.scss';
-import logo from './assets/images/logo.svg';
-
-const img = new Image();
-img.src = logo;
-document.getElementById('tr-logo-container')?.appendChild(img);
